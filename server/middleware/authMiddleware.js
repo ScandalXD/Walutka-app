2db82ae49a76d80a48242dbeed46887661b2b541
kubisma <213828029+kubisma@users.noreply.github.com>
2025-12-28 11:53:23 +0100

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 function authMiddleware(req, res, next) {
-  const authHeader = req.headers.authorization; // "Bearer token"
+  const authHeader = req.headers.authorization;
   if (!authHeader) return res.status(401).json({ message: 'Brak tokenu' });
 
   const token = authHeader.split(' ')[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = payload; // { userId, email }
+    req.user = payload;
     next();
   } catch (err) {
     return res.status(401).json({ message: 'Nieprawidłowy token' });
